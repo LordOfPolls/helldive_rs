7b671766f2cd9d9c8274ca199a9131bb30dbece6
polls <dev@lordofpolls.com>
2024-03-14 22:09:44 +0000

    feat: raise error on invalid war id or response

--- src/error.rs
+++ src/error.rs
@@ -1,10 +1,14 @@
 use std::error::Error;
+use reqwest::Response;
+use reqwest::StatusCode;
 use std::fmt;
 
 #[derive(Debug)]
 pub enum HelldiversError {
     RequestError(reqwest::Error),
     JsonError(serde_json::Error),
+    APIError(String),
+    InvalidWarId(String),
 }
 
 impl fmt::Display for HelldiversError {
@@ -12,6 +16,8 @@ impl fmt::Display for HelldiversError {
         match self {
             HelldiversError::RequestError(err) => write!(f, "Request error: {}", err),
             HelldiversError::JsonError(err) => write!(f, "JSON deserialization error: {}", err),
+            HelldiversError::InvalidWarId(message) => write!(f, "Invalid war ID: {}", message),
+            HelldiversError::APIError(message) => write!(f, "API error: {}", message),
         }
     }
 }
@@ -21,6 +27,8 @@ impl Error for HelldiversError {
         match self {
             HelldiversError::RequestError(err) => Some(err),
             HelldiversError::JsonError(err) => Some(err),
+            HelldiversError::InvalidWarId(_) => None,
+            HelldiversError::APIError(_) => None,
         }
     }
 }
@@ -34,5 +42,15 @@ impl From<reqwest::Error> for HelldiversError {
 impl From<serde_json::Error> for HelldiversError {
     fn from(err: serde_json::Error) -> Self {
         HelldiversError::JsonError(err)
+    }
+}
+
+impl From<Response> for HelldiversError {
+    fn from(response: Response) -> Self {
+        if response.status() == StatusCode::BAD_REQUEST {
+            HelldiversError::InvalidWarId(format!("Invalid war ID: {}", response.url()))
+        } else {
+            HelldiversError::APIError(format!("API error: {}", response.status()))
+        }
     }
 }
