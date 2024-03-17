5d0cbd42ecb3a54dfc7a92cc3a2e25058693ddb6
LordOfPolls <dev@lordofpolls.com>
2024-03-17 09:07:13 +0000

    feat: add get sectors and get factions

--- src/utils.rs
+++ src/utils.rs
@@ -1,10 +1,11 @@
 use std::collections::HashMap;
-use crate::{PlanetStatus, Status};
+use crate::{PlanetStatus, Status, WarInfo, get_sector_name, Faction, get_faction_name};
+use crate::models::Sector;
 
 /// Get the total player count for a status
 ///
 /// Arguments:
-///  status: Status - The status to get the player count for
+///     status: Status - The status to get the player count for
 pub fn get_total_player_count(status: &Status) -> i64 {
     status.planet_status.iter().map(|ps| ps.players).sum()
 }
@@ -12,8 +13,8 @@ pub fn get_total_player_count(status: &Status) -> i64 {
 /// Get the top planets by player count
 ///
 /// Arguments:
-///  status: &Status - The status to get the top planets from
-///  count: usize - The number of top planets to get
+///     status: &Status - The status to get the top planets from
+///     count: usize - The number of top planets to get
 pub fn get_top_planets_by_player_count(status: &Status, count: usize) -> Vec<(&PlanetStatus, i64)> {
     let mut planet_players: Vec<(&PlanetStatus, i64)> = status.planet_status.iter().map(|ps| (ps, ps.players)).collect();
     planet_players.sort_by(|a, b| b.1.cmp(&a.1));
@@ -23,7 +24,7 @@ pub fn get_top_planets_by_player_count(status: &Status, count: usize) -> Vec<(&P
 /// Get the faction distribution
 ///
 /// Arguments:
-///  status: &Status - The status to get the faction distribution from
+///     status: &Status - The status to get the faction distribution from
 pub fn get_faction_distribution(status: &Status) -> HashMap<i64, i64> {
     let mut distribution = HashMap::new();
     for ps in &status.planet_status {
@@ -31,3 +32,29 @@ pub fn get_faction_distribution(status: &Status) -> HashMap<i64, i64> {
     }
     distribution
 }
+
+/// Get the sectors from a WarInfo
+///
+/// Arguments:
+///     status: &WarInfo - The WarInfo to get the sectors from
+pub fn get_sectors(status: &WarInfo) -> Vec<Sector> {
+    // intentionally does not use the cached sectors in case there are new sectors in the WarInfo
+    let mut sectors = status.planet_infos.iter().map(|pi| pi.sector).collect::<Vec<_>>();
+    sectors.sort();
+    sectors.dedup();
+    sectors.into_iter().map(|s| Sector {
+        id: s, name: get_sector_name(s).unwrap_or_default(), planets: status.planet_infos.iter().filter(|pi| pi.sector == s).map(|pi| pi.index).collect()
+    }).collect()
+}
+
+/// Get the factions from a Status
+///
+/// Arguments:
+///    status: &Status - The Status to get the factions from
+pub fn get_factions(status: &Status) -> Vec<Faction> {
+    let mut factions = status.planet_status.iter().map(|ps| ps.owner).collect::<Vec<_>>();
+    factions.sort();
+    factions.dedup();
+    factions.into_iter().map(|f| Faction { id: f, name: get_faction_name(f).unwrap_or_default() }).collect()
+}
+
